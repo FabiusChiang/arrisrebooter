38911b74b110b36f83efd6eaa57b487e2a91d65b
Fabius <Fabius.Jiang@ms.com>
2020-07-01 14:34:40 -0500

    automatically delete old log files

--- launch.sh
+++ launch.sh
@@ -8,6 +8,9 @@ cd ${folderOfCurrentScript}
 logfileName=`date +"%m-%d-%y"`
 logfileName="logs/${logfileName}.log"
 
-# nvm use 10
+## nvm use 10
 echo `date` | tee -a ${logfileName}
 node index.js >> ${logfileName}
+
+## Delete log files older than 30 days
+find ./logs -name "*.log" -type f -mtime +30 -exec rm -f {} \;
